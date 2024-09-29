4d29ffd60b115fdedb21aaaa1c01eae60778fea4
huesosRedkostniy <ltdx@bk.ru>
2024-09-29 14:47:05 +0500

    Add markup for pages

---
+++ src/components/placeCard/placeCard.tsx
@@ -0,0 +1,63 @@
+type PlaceCardProps = {
+  isPremium: boolean;
+  imageLink: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+}
+
+export function PlaceCard({isPremium, imageLink, price, rating, title, type}: PlaceCardProps): JSX.Element {
+  return (
+    <article className="cities__place-card place-card">
+      {isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img
+            className="place-card__image"
+            src={imageLink}
+            width={260}
+            height={200}
+            alt="Place image"
+          />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">€{price}</b>
+            <span className="place-card__price-text">/&nbsp;night</span>
+          </div>
+          <button
+            className="place-card__bookmark-button button"
+            type="button"
+          >
+            <svg
+              className="place-card__bookmark-icon"
+              width={18}
+              height={19}
+            >
+              <use xlinkHref="#icon-bookmark"/>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${rating}%`}}/>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">
+            {title}
+          </a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
+  );
+}
